e743566cf30a3c2ce6c4492a2c544bc32c6d6e14
Ailton Santos <ailtonsanntos@gmail.com>
2022-06-04 13:35:53 -0300

    aula 16 anotacao wait pages map

--- src/test/java/br/com/chronosAcademy/core/Driver.java
+++ src/test/java/br/com/chronosAcademy/core/Driver.java
@@ -2,13 +2,19 @@ package br.com.chronosAcademy.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Driver {
     private static WebDriver driver; // Static - Algo estático
+    private static WebDriverWait wait;
 
     //Ele é construtor pq ele é public e tem o nome da classe
     public Driver(String navegador){
@@ -35,12 +41,17 @@ public class Driver {
                 driver = new ChromeDriver(); //Abrindo o Chrome
                 break;
         }
-
-
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();  //Maximizando a tela
-
     }
 
+    public void static visibilityOf(WebElement element){
+        wait.until(ExpectedCondition.visibilityOf(element));
+    }
+
+    public void static invisibilityOf(WebElement element){
+        wait.until(ExpectedCondition.invisibilityOf(element));
+    }
     public static WebDriver getDriver(){
         return driver;
     }
